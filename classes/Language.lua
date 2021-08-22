6f3b1adb0eead74603f649ca4e8bacf0d9371cb2
Paul Layne <playne@v0.design>
2021-08-22 12:04:32 -0400

    fix(General): disable the maintainMaxSpeedfix(General): added support for language files, updated downloader to download from a txt file less updating the pastebin file.

--- classes/Language.lua
+++ classes/Language.lua
@@ -9,18 +9,15 @@ local Language = {
     end,
 
     loadLanguageByFile = function(self, languageFile)
-        print("Loading '"..languageFile.."' file")
         local file = fs.open(languageFile,"r")
         local list = file.readAll()
         file.close()
 
         text = textutils.unserialise(list)
     end,
-    loadLanguageById = function(self, languageId)    
-        print("Language Requested: "..languageId)
+    loadLanguageById = function(self, languageId)
         local fileName = "/extreme-reactors-control/lang/"..languageId..".txt"
-        print("Using File: "..fileName)
-        self.loadLanguageByFile(fileName)
+        self:loadLanguageByFile(fileName)
     end
 }
 
