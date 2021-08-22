6815f0687fcebdab2c4daa6cc0c34fa2380323df
Paul Layne <playne@v0.design>
2021-08-22 13:56:49 -0400

    fix(General): disable the maintainMaxSpeedfix(General): added support for language files, updated downloader to download from a txt file less updating the pastebin file.

--- classes/Language.lua
+++ classes/Language.lua
@@ -16,8 +16,8 @@ local Language = {
         local file = fs.open(languageFile,"r")
         local list = file.readAll()
         file.close()
-
-        text = textutils.unserialise(list)
+        unsortedList = textutils.unserialise(list)
+        text = spairs(unsortedList)
     end,
 
     loadLanguageById = function(self, languageId)
@@ -28,11 +28,35 @@ local Language = {
 
 function _G.newLanguageById(languageId)
     local language = {}
-    setmetatable(language,{__index=Language})
-    language:loadLanguageById(languageId)
+    setmetatable(language,{__index=Language})  
+    language:loadLanguageById(languageId)  
+    print(language:getText("loadedLanguage"))
     return language
 end
 
+-- copied from https://stackoverflow.com/questions/15706270/sort-a-table-in-lua
+function spairs(t, order)
+    -- collect the keys
+    local keys = {}
+    for k in pairs(t) do keys[#keys+1] = k end
+
+    -- if order function given, sort by it by passing the table and keys a, b,
+    -- otherwise just sort the keys 
+    if order then
+        table.sort(keys, function(a,b) return order(t, a, b) end)
+    else
+        table.sort(keys)
+    end
+
+    -- return the iterator function
+    local i = 0
+    return function()
+        i = i + 1
+        if keys[i] then
+            return keys[i], t[keys[i]]
+        end
+    end
+end
 
 
 
