2a30bb057085655388514f701be30fc9141969b5
Paul Layne <playne@v0.design>
2021-08-23 18:45:55 -0400

    fix(General): added support for language files, updated downloader to download from a txt file less updating the pastebin file.

--- config/input.lua
+++ config/input.lua
@@ -1,17 +1,6 @@
 function formatNumberComma(number)
-  --thanks to https://stackoverflow.com/questions/10989788/format-integer-in-lua answer by Bert Kiers
-
-
-    local i, j, minus, int, fraction = tostring(number):find('([-]?)(%d+)([.]?%d*)')
-  
-    -- reverse the int-string and append a comma to all blocks of 3 digits
-    int = int:reverse():gsub("(%d%d%d)", "%1,")
-  
-    -- reverse the int-string back remove an optional comma and put the 
-    -- optional minus and fractional part back
-
-    local finalOutput =  minus .. int:reverse():gsub("^,", "") .. fraction
-
+    local finalOutput =  format_int(number)
+    print(finalOutput)
     --update to use correct language delimiter.
     finalOutput = string.gsub(finalOutput, ".", "frac")
     finalOutput = string.gsub(finalOutput, ",", "thou")
@@ -20,4 +9,16 @@ function formatNumberComma(number)
     finalOutput = string.gsub(finalOutput, "thou", _G.language:getText("thousandsDelimiter"))
 
     return finalOutput
+end
+
+function format_int(number)
+  --thanks to https://stackoverflow.com/questions/10989788/format-integer-in-lua answer by Bert Kiers
+  local i, j, minus, int, fraction = tostring(number):find('([-]?)(%d+)([.]?%d*)')
+
+  -- reverse the int-string and append a comma to all blocks of 3 digits
+  int = int:reverse():gsub("(%d%d%d)", "%1,")
+
+  -- reverse the int-string back remove an optional comma and put the 
+  -- optional minus and fractional part back
+  return minus .. int:reverse():gsub("^,", "") .. fraction
 end
