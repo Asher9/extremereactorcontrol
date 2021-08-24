0734a012f1d65f84e44fb0db7d83abbdae382a57
Paul Layne <playne@v0.design>
2021-08-24 19:20:18 -0400

    fix(General): fix for latest Mekanism v10.0.21.x version

--- start/start.lua
+++ start/start.lua
@@ -101,7 +101,7 @@ function _G.refreshOptionList()
 	optionList["language"] = lang
 end
 
---Saves all data basck to the options.txt file
+--Saves all data back to the options.txt file
 function _G.saveOptionFile()
 	debugOutput("Saving Option File")
 	--Refresh option list
