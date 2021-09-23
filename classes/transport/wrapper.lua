c6948edf03ad8c7221aee32b3ad7e4c6a3e4303a
Paul Layne <playne@v0.design>
2021-09-22 21:01:39 -0400

    feat(Wireless): added wireless transmit functionality

--- classes/transport/wrapper.lua
+++ classes/transport/wrapper.lua
@@ -11,23 +11,14 @@ shell.run(binPath.."reactorturbine.lua")
 shell.run(binPath.."startup.lua")
 
 function _G.newMessage(type, data, location)
-    debugOutput("Creating new Wrapper Class")
-
     local message = {}
     setmetatable(message,{__index = Wrapper})
-    
-    if data == nil then
-        debugOutput("MISSING data object. This is going to break!")
-    end
-
-    debugOutput("Settings Name -> ".. type)
     message.data = data
     message.type = type
 
-    return  textutils.unserialise(message)
+    return textutils.unserialise(message)
 end
 
 function _G.readMessage(input)
-    debugOutput("Creating new Wrapper Class")
     return textutils.unserialise(input)
 end
